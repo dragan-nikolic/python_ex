6dddda2a083b96e6efc73743a8d15ef06bc1a99a
Dragan Nikolic <dragan.nikolic@inspirebrands.com>
2024-01-10 15:55:17 +0100

    rearrange files

--- yaml/test_yaml_read.py
+++ yaml/test_yaml_read.py
@@ -1,8 +1,11 @@
 import yaml
 import pprint
 
-f = open("test.yml")
-data = yaml.load(f)
+#f = open("test.yml")
+f = open("table.yml")
+data = yaml.load(f, Loader=yaml.Loader)
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(data)
+
+print(data['column_distinct_values'][1])
